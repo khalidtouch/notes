62d60fb122f7e6658013cefdd405344719a5fa47
khalidtouch <khalidhulabaloo9@gmail.com>
2020-05-11 23:33:54 +0200

    created a Word data class to hold each item

---
+++ app/src/main/java/com/example/notes/Word.java
@@ -0,0 +1,11 @@
+package com.example.notes;
+
+public class Word {
+    private String textString;
+
+    public Word(String textString){
+        this.textString = textString;
+    }
+
+    public String getText(){return textString;}
+}
