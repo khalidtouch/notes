3ed5f0d2398f884cde5a8cfc8300d4485e46d186
khalidtouch <khalidhulabaloo9@gmail.com>
2020-05-12 19:35:22 +0200

    added a menu item

--- app/src/main/java/com/example/notes/AddNoteActivity.java
+++ app/src/main/java/com/example/notes/AddNoteActivity.java
@@ -1,8 +1,12 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class AddNoteActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class AddNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_note, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.done:
+                Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                //
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
